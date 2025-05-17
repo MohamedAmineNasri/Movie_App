c7296b6bf480e9aca75e806083c733d780a77534
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2025-05-17 18:32:21 +0100

    feat: implement movie search functionality

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,30 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { StatusBar } from "react-native";
+import { useEffect } from "react";
+import * as NavigationBar from 'expo-navigation-bar';
+import * as SystemUI from 'expo-system-ui';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+  'setBackgroundColorAsync is only available on Android',
+  'setButtonStyleAsync is only available on Android',
+])
 export default function RootLayout() {
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync('#1A1A2E');
+    NavigationBar.setButtonStyleAsync('light');  
+    
+    SystemUI.setBackgroundColorAsync('#1A1A2E');
+  }, []);
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
